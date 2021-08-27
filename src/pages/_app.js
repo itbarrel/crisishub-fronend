97c6f59bfa25a5cbbba02d2427daa87498074ff0
Muhammad Ali <MAAKmaker@gmail.com>
2021-08-27 19:54:23 +0500

    fix proptype issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import '../assets/styles/globals.css'
 import { wrapper } from '../store'
 import '../assets/styles/scss/styles.scss'
-
+import PropTypes from 'prop-types'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default wrapper.withRedux(MyApp)
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
+};
+
+
