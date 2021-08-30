8b12b11c3e0dd6212c5a0d3f649fb403c1355af5
Muhammad Ali <maakmaker@gmail.com>
2021-08-30 12:18:35 +0500

    internationlization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,18 @@ import '../assets/styles/globals.css'
 import { wrapper } from '../store'
 import '../assets/styles/scss/styles.scss'
 import PropTypes from 'prop-types'
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LocaleProvider locale={enUS}>
+      <Component {...pageProps} />
+    </LocaleProvider>
+  )
 }
 
 export default wrapper.withRedux(MyApp)
-
 MyApp.propTypes = {
   Component: PropTypes.any,
   pageProps: PropTypes.any,
