d918c6c761e8d79d68e47498dd24563375bbec21
Muhammad Ali <MAAKmaker@gmail.com>
2021-09-16 20:25:17 +0500

    isauthenticate function fix

--- src/utils/is-authenticate.js
+++ src/utils/is-authenticate.js
@@ -1,12 +1,10 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { CookieService } from "../services/storage.service";
 
-const IsAuthenticate = () => {
-    const { isauthenticate } = useSelector(({ auth }) => auth);
+export const IsAuthenticate = () => {
+    // const { isauthenticate } = useSelector(({ auth }) => auth);
     const token = CookieService.getToken();
 
-    const validate = isauthenticate && token;
+    const validate = token ? true : false;
     return validate;
 };
-
-export default IsAuthenticate;
