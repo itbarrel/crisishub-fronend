5e747c021bc74da9028081850bb6b670e6b829ec
Muhammad Ali <MAAKmaker@gmail.com>
2021-09-23 20:16:53 +0500

    add change language toggle

--- src/layouts/Topbar/languageData.js
+++ src/layouts/Topbar/languageData.js
@@ -1,10 +1,10 @@
 const languageData = [
-  {
-    languageId: 'russian', // Unique ID for language
-    locale: 'ru',         // locale name to link with the previously named locale
-    name: 'Русский',      // Display name of the language
-    icon: 'ru'            // To choose the flag of the country
-  },
+  // {
+  //   languageId: 'russian', // Unique ID for language
+  //   locale: 'ru',         // locale name to link with the previously named locale
+  //   name: 'Русский',      // Display name of the language
+  //   icon: 'ru'            // To choose the flag of the country
+  // },
   {
     languageId: 'english',
     locale: 'en',
@@ -12,35 +12,41 @@ const languageData = [
     icon: 'us'
   },
   {
-    languageId: 'chinese',
-    locale: 'zh',
-    name: 'Chinese',
+    languageId: 'dutch',
+    locale: 'dh',
+    name: 'Dutch',
     icon: 'cn'
   },
-  {
-    languageId: 'spanish',
-    locale: 'es',
-    name: 'Spanish',
-    icon: 'es'
-  },
-  {
-    languageId: 'french',
-    locale: 'fr',
-    name: 'French',
-    icon: 'fr'
-  },
-  {
-    languageId: 'italian',
-    locale: 'it',
-    name: 'Italian',
-    icon: 'it'
-  },
-  {
-    languageId: 'saudi-arabia',
-    locale: 'ar',
-    name: 'Arabic',
-    icon: 'sa'
-  }
+  // {
+  //   languageId: 'chinese',
+  //   locale: 'zh',
+  //   name: 'Chinese',
+  //   icon: 'cn'
+  // },
+  // {
+  //   languageId: 'spanish',
+  //   locale: 'es',
+  //   name: 'Spanish',
+  //   icon: 'es'
+  // },
+  // {
+  //   languageId: 'french',
+  //   locale: 'fr',
+  //   name: 'French',
+  //   icon: 'fr'
+  // },
+  // {
+  //   languageId: 'italian',
+  //   locale: 'it',
+  //   name: 'Italian',
+  //   icon: 'it'
+  // },
+  // {
+  //   languageId: 'saudi-arabia',
+  //   locale: 'ar',
+  //   name: 'Arabic',
+  //   icon: 'sa'
+  // }
 
 ];
 export default languageData;
--- src/lngProvider/index.js
+++ src/lngProvider/index.js
@@ -5,9 +5,11 @@ import itLang from "./entries/it_IT";
 import esLang from "./entries/es_ES";
 import frLang from "./entries/fr_FR";
 import ruLang from "./entries/ru_RU";
+import nlLang from "./entries/nl_BE";
 
 const AppLocale = {
   en: enLang,
+  dh: nlLang,
   zh: zhLang,
   ar: arLang,
   it: itLang,
