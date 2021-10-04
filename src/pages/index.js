098511d64c62e0057a0e51fa32415c700069f87c
Saaad <saaadii.mahmood.7@gmail.com>
2021-10-04 19:54:41 +0500

    changes in middle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
-import react, { memo } from "react";
+import react, { memo, useEffect } from "react";
 import asyncComponent from "../utils/asyncComponent";
+import Router from 'next/router'
+import { CookieService } from "../services/storage.service";
 
 const Login = asyncComponent(() => import("../pages/auth/login"));
 
 const Home = memo(() => {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  const token = CookieService.getToken()
+
+  useEffect(() => {
+    if (token) { Router.push('/secure/dashboard') }
+    console.log(token)
+  }, [token]);
+
+  return <Login />
 });
 
 Home.displayName = Home;
